62f95d43823b8923e86af68ee7091c65bf7927d2
Nelio Alves <acenelio@gmail.com>
2018-10-04 18:18:47 -0300

    Initial implementation

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Course.Entities;
+using Course.Services;
 
-namespace Course
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            ProductService ps = new ProductService();
+
+            double sum = ps.FilteredSum(list, p => p.Name[0] == 'T');
+
+            Console.WriteLine("Sum = " + sum.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
---
+++ Course/Services/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Course.Entities;
+
+namespace Course.Services {
+    class ProductService {
+
+        public double FilteredSum(List<Product> list, Func<Product, bool> criteria) {
+            double sum = 0.0;
+            foreach (Product p in list) {
+                if (criteria.Invoke(p)) {
+                    sum += p.Price;
+                }
+            }
+            return sum;
+        }
+    }
+}
